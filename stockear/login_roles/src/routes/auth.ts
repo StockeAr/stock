d410d1ae32ffc6beb7d42dc308455f43f9be3a3a
juantapia1999 <tapiahuanca.juanluis@gmail.com>
2020-11-30 16:38:47 -0300

    algo

--- stockear/login_roles/src/routes/auth.ts
+++ stockear/login_roles/src/routes/auth.ts
@@ -1,10 +1,10 @@
-import {Router} from 'express';
+import { Router } from 'express';
 import AuthController from '../controller/AuthController';
-import {checkJwt} from '../middleware/jwt';
+import { checkJwt } from '../middleware/jwt';
 
-const router=Router();
+const router = Router();
 //login
-router.post('/login',AuthController.login);
+router.post('/login', AuthController.login);
 //cambiar la contraseña, para ademas restringir sola al admin, se debe importar el checkRol
-router.post('/change-password',[checkJwt],AuthController.changePassword)
+router.post('/change-password', [checkJwt], AuthController.changePassword)
 export default router;
