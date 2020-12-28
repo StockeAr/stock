a46e7e5a13b4662857c2ea0c1289bfec8768cdfc
juantapia1999 <tapiahuanca.juanluis@gmail.com>
2020-12-28 09:42:28 -0300

    deploy heroku

--- stockear/server.js
+++ stockear/server.js
@@ -3,16 +3,18 @@ const path = require('path');
 
 const app = express();
 
-
+const PORT=process.env.PORT || 8080;
 // Serve only the static files form the dist directory
 // Replace the '/dist/<to_your_project_name>'
 app.use(express.static(__dirname+'/dist/stockear'));
 
 
-app.get('/',function(req,res){
+app.get('/*',function(req,res){
     // Replace the '/dist/<to_your_project_name>/index.html'
     res.sendFile(path.join(__dirname+'/dist/stockear/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(PORT,()=>{
+    console.log('Servidor en el puerto : '+PORT);
+});
